b01cf40bda15f6b38ec0dafc7ff17c7fde39cde5
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-28 22:19:35 -0400

    Fixed issue where Products got deleted with cat

--- models/index.js
+++ models/index.js
@@ -5,15 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: "CASCADE"
-});
+Product.belongsTo(Category, { foreignKey: 'category_id', onDelete: 'cascade', hooks:true});
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-});
+Category.hasMany(Product, { onDelete: 'set null', hooks:true });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
