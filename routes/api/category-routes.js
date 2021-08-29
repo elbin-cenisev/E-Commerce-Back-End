be5cb562eb34b4d9bd47597031aecef40b35be9e
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-28 20:20:30 -0400

    Added POST '/' Category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  // create a new tag
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
